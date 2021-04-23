1c4d597b930062c836a4b1257c07a9e50d899d7f
Adby Santos <blackleg15@hotmail.com>
2021-04-23 10:02:31 -0300

    Readability + errorWidget isn't a function anymore

--- lib/src/error_mixin/error_mixin_with_widget.dart
+++ lib/src/error_mixin/error_mixin_with_widget.dart
@@ -2,17 +2,17 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_triple/flutter_triple.dart';
 
 mixin ErrorMixinWithWidget<T extends StatefulWidget> on State<T> {
-  Store<Object, Object> get store;
-  Widget get onStateWidgetOnError;
-  Widget Function(Object error) get onErrorWidget;
+  Store<Object, Object> get tripleStore;
+  Widget get onStateWidgetForErrorMixin;
+  Widget get onErrorWidgetForErrorMixin;
   late Widget finalPageForErrorMixin;
 
   @override
   void initState() {
     super.initState();
     finalPageForErrorMixin = TripleBuilder(
-      store: store,
-      builder: (context, triple) => triple.error != null ? onErrorWidget(triple.error!) : onStateWidgetOnError,
+      store: tripleStore,
+      builder: (context, triple) => triple.error != null ? onErrorWidgetForErrorMixin : onStateWidgetForErrorMixin,
     );
   }
 }
--- lib/src/loading_mixin/loading_mixin.dart
+++ lib/src/loading_mixin/loading_mixin.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_triple/flutter_triple.dart';
 
 mixin LoadingMixin<T extends StatefulWidget> on State<T> {
-  Store<Object, Object> get store;
-  Widget get onStateWidget;
-  Widget get onLoadingWidget => const Center(child: const CircularProgressIndicator());
+  Store<Object, Object> get tripleStore;
+  Widget get onStateWidgetForLoadingMixin;
+  Widget get onLoadingWidgetForLoadingMixin;
   late Widget finalPageForLoadingMixin;
 
   @override
   void initState() {
     super.initState();
     finalPageForLoadingMixin = TripleBuilder(
-      store: store,
-      builder: (context, triple) => triple.isLoading ? onLoadingWidget : onStateWidget,
+      store: tripleStore,
+      builder: (context, triple) => triple.isLoading ? onLoadingWidgetForLoadingMixin : onStateWidgetForLoadingMixin,
     );
   }
 }
