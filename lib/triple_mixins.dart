949231300b20b18603af98717a44f196493449f7
Adby Santos <blackleg15@hotmail.com>
2021-04-23 01:32:05 -0300

    Turning this package into a library

--- lib/triple_mixins.dart
+++ lib/triple_mixins.dart
@@ -1,3 +1,5 @@
-export 'mixins/loading_mixin/loading_mixin.dart' show LoadingMixin;
-export 'mixins/error_mixin/error_mixin_with_snackbar.dart' show ErrorMixinWithSnackbar;
-export 'mixins/error_mixin/error_mixin_with_widget.dart' show ErrorMixinWithWidget;
+library triple_mixins;
+
+export 'src/loading_mixin/loading_mixin.dart';
+export 'src/error_mixin/error_mixin_with_snackbar.dart';
+export 'src/error_mixin/error_mixin_with_widget.dart';
